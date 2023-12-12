85bf4113730d2e164479e78552216dc41fcadea0
Lizandra-Silva <lizandra.silva@evosystems.com.br>
2023-12-12 20:26:22 -0300

    Criar casos de uso de register student e authenticate student

---
+++ src/domain/forum/application/use-cases/authenticate-student.ts
@@ -0,0 +1,58 @@
+import { Either, left, right } from '@/core/either'
+import { Injectable } from '@nestjs/common/decorators'
+import { Student } from '../../enterprise/entities/student'
+import { StudentsRepository } from '../repositories/students-repository'
+import { HashGenerator } from '../cryptography/hash-generator'
+import { StudentAlreadyExistsError } from './errors/student-already-exists-error'
+import { HashComparer } from '../cryptography/hash-comparer'
+import { Encrypter } from '../cryptography/encrypter'
+import { WrongCredentalsError } from './errors/wrong-credentials-error'
+
+interface AuthenticateStudentUseCaseRequest {
+  email: string
+  password: string
+}
+
+type AuthenticateStudentUseCaseResponse = Either<
+  WrongCredentalsError,
+  {
+    accessToken: string
+  }
+>
+
+@Injectable()
+export class AuthenticateStudentUseCase {
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private hshComparer: HashComparer,
+    private encrypter: Encrypter,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateStudentUseCaseRequest): Promise<AuthenticateStudentUseCaseResponse> {
+    const student = await this.studentsRepository.findByEmail(email)
+
+    if (!student) {
+      return left(new WrongCredentalsError())
+    }
+
+    const isPasswordValid = await this.hshComparer.compare(
+      password,
+      student.password,
+    )
+
+    if (!isPasswordValid) {
+      return left(new WrongCredentalsError())
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: student.id.toString(),
+    })
+
+    return right({
+      accessToken,
+    })
+  }
+}
