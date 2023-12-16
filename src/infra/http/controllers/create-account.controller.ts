7a4d28ea79038aa55b0393f03c8a43075fbe5f95
Lizandra-Silva <lizandra.silva@evosystems.com.br>
2023-12-16 11:02:10 -0300

    Tratando erro nos controllers

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Post, Body, UsePipes } from '@nestjs/common'
+import {
+  Controller,
+  Post,
+  Body,
+  UsePipes,
+  ConflictException,
+  BadRequestException,
+} from '@nestjs/common'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 import { z } from 'zod'
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
+import { StudentAlreadyExistsError } from '@/domain/forum/application/use-cases/errors/student-already-exists-error'
 
 const createAccountBodySchema = z.object({
   name: z.string(),
@@ -27,7 +35,14 @@ export class CreateAccountController {
     })
 
     if (result.isLeft()) {
-      throw new Error()
+      const error = result.value
+
+      switch (error.constructor) {
+        case StudentAlreadyExistsError:
+          throw new ConflictException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
     }
   }
 }
