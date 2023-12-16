7e4bcf5ed6c5b1f499c23d26bfc48919d9e43ec7
Lizandra-Silva <lizandra.silva@evosystems.com.br>
2023-12-16 11:42:09 -0300

    Criando EnvModule

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -1,21 +1,22 @@
 import { Module } from '@nestjs/common'
 import { PassportModule } from '@nestjs/passport'
 import { JwtModule } from '@nestjs/jwt'
-import { ConfigService } from '@nestjs/config'
-import { Env } from '@/infra/env'
 import { JWTStrategy } from './jwt.strategy'
 import { APP_GUARD } from '@nestjs/core'
 import { JwtAuthGuard } from './jwt-auth.guard'
+import { EnvModule } from '../env/env.module'
+import { EnvService } from '../env/env.service'
 
 @Module({
   imports: [
     PassportModule,
     JwtModule.registerAsync({
-      inject: [ConfigService],
+      imports: [EnvModule],
+      inject: [EnvService],
       global: true,
-      useFactory(config: ConfigService<Env, true>) {
-        const privateKey = config.get('JWT_SECRET_KEY', { infer: true })
-        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
+      useFactory(env: EnvService) {
+        const privateKey = env.get('JWT_SECRET_KEY')
+        const publicKey = env.get('JWT_PUBLIC_KEY')
 
         return {
           signOptions: { algorithm: 'RS256' },
@@ -26,11 +27,12 @@ import { JwtAuthGuard } from './jwt-auth.guard'
     }),
   ],
   providers: [
+    JWTStrategy,
+    EnvService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
     },
-    JWTStrategy,
   ],
 })
 export class AuthModule {}
